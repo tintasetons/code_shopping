2103f55d603eed9ef376904ff71a53dbd8677785
Walter <pbtintas@gmail.com>
2019-06-24 21:28:03 -0300

    Cadastrando entradas de estoque

--- app/Http/Controllers/Api/ProductInputController.php
+++ app/Http/Controllers/Api/ProductInputController.php
@@ -3,9 +3,9 @@
 namespace CodeShopping\Http\Controllers\Api;
 
 use CodeShopping\Http\Controllers\Controller;
+use CodeShopping\Http\Requests\ProductInputRequest;
 use CodeShopping\Http\Resources\ProductInputResource;
 use CodeShopping\Models\ProductInput;
-use Illuminate\Http\Request;
 
 class ProductInputController extends Controller
 {
@@ -13,18 +13,20 @@ class ProductInputController extends Controller
     public function index()
     {
         return  ProductInputResource::collection(ProductInput::with('product')->paginate());
-
-
     }
 
-    public function store(Request $request)
+    public function store(ProductInputRequest $request)
     {
-        //
+        $input = ProductInput::create($request->all());
+        $product = $input->product;
+        $product->stock -= $input->amount;
+        $product->save();
+        return new ProductInputResource($input);
     }
 
 
-    public function show(ProductInput $productInput)
+    public function show(ProductInput $input)
     {
-        //
+        return new ProductInputResource($input);
     }
 }
