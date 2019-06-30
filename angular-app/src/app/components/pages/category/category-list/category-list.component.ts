e0b1b2d9550505ba4f33a156cda5058b5251a2ca
Walter <pbtintas@gmail.com>
2019-06-29 21:08:34 -0300

    Formatação de datas

--- angular-app/src/app/components/pages/category/category-list/category-list.component.ts
+++ angular-app/src/app/components/pages/category/category-list/category-list.component.ts
@@ -4,7 +4,7 @@ import {HttpClient} from "@angular/common/http";
 @Component({
   selector: 'category-list',
   templateUrl: './category-list.component.html',
-  styleUrls: ['./category-list.component.css']
+  styleUrls: ['../../login/login.component.css']
 })
 export class CategoryListComponent implements OnInit {
 
@@ -16,13 +16,13 @@ export class CategoryListComponent implements OnInit {
 
   ngOnInit() {
     const token = window.localStorage.getItem('token');
-    this.http.get<Array<any>>('http://localhost:8000/api/categories',{
+    this.http.get<{data:Array<any>}>('http://localhost:8000/api/categories',{
         headers: {
           'Authorization':`Bearer ${token}`
         }
 
       })
-      .subscribe(data => this.categories = data);
+      .subscribe(response => this.categories = response.data);
   }
 
 }
