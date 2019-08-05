8b8f2c751a3625e54d8cfe900f448ce01936e369
tintasetons <tintasetons@gmail.com>
2019-08-05 19:09:22 -0300

    Criando logout

--- angular-app/src/app/services/auth.service.ts
+++ angular-app/src/app/services/auth.service.ts
@@ -30,7 +30,7 @@ export class AuthService {
 
   setToken(token: string) {
     this.setUserFromToken(token);
-    window.localStorage.setItem(TOKEN_KEY, token);
+    token ? window.localStorage.setItem(TOKEN_KEY, token) : window.localStorage.removeItem(TOKEN_KEY);
   }
 
   private setUserFromToken(token: string) {
@@ -46,8 +46,17 @@ export class AuthService {
     return window.localStorage.getItem(TOKEN_KEY);
   }
 
-  isAuth(){
+  isAuth() {
     const token = this.getToken();
     return !new JwtHelperService().isTokenExpired(token, 30);
+  }
+
+  logout(): Observable<any> {
+    return this.http.post<{ token: string }>('http://localhost:8000/api/logout', {})
+      .pipe(
+        tap(() => {
+          this.setToken(null)
+        })
+      );
   }
 }
