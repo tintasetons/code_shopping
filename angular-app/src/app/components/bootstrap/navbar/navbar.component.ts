7795f8fc9141794c135602c1c0f17fec68058679
tintasetons <tintasetons@gmail.com>
2019-08-05 18:41:49 -0300

    Criando componente para menu de navegação

--- angular-app/src/app/components/bootstrap/navbar/navbar.component.ts
+++ angular-app/src/app/components/bootstrap/navbar/navbar.component.ts
@@ -1,13 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import {AuthService} from "../../../services/auth.service";
 
 @Component({
-  selector: 'app-navbar',
+  selector: 'navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
+  constructor(public authService: AuthService) { }
 
   ngOnInit() {
   }
